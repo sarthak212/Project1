09d0922cff77da60a35ef77d1e1c955f412a7107
ShoaibIN <17bcs052@smvdu.ac.in>
2020-06-10 17:58:44 +0530

    More changes in product page

---
+++ public/javascripts/index.js
@@ -0,0 +1,28 @@
+/* eslint-disable prefer-arrow-callback, no-var, no-tabs */
+$(document).ready(function () {
+  // Add specific code to this theme here
+  $(".carousel").swipe({
+
+    swipe: function (event, direction, distance, duration, fingerCount, fingerData) {
+
+      if (direction == 'left') $(this).carousel('next');
+      if (direction == 'right') $(this).carousel('prev');
+
+    },
+    allowPageScroll: "vertical"
+  });
+
+  //Product Details image slider
+  $('#etalage').etalage({
+    smallthumbs_position: 'left',
+    thumb_image_width: 380,
+    thumb_image_height: 480,
+    source_image_width: 900,
+    source_image_height: 1100
+  });
+
+  $('#etalage1').etalage({
+    thumb_image_width: 350,
+    thumb_image_height: 400,
+  });
+});
